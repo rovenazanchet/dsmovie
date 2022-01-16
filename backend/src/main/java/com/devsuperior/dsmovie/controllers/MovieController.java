64d907cd7ff07f349c7102fe63669892eab3f1cd
Rovena Zanchet <rovenazanchet@gmail.com>
2022-01-16 17:18:29 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -12,7 +12,7 @@ import com.devsuperior.dsmovie.dto.MovieDTO;
 import com.devsuperior.dsmovie.services.MovieService;
 
 @RestController
-@RequestMapping(value = "/Movies")
+@RequestMapping(value = "/movies")
 public class MovieController {
 	
 	@Autowired
